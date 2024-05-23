0d4a4b82d33834edb6062f096cafe84b74ca202b
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2024-05-22 23:11:21 -0300

    modules Database and Routers

--- training-node-js/01-fundamentos-nodejs/src/server.js
+++ training-node-js/01-fundamentos-nodejs/src/server.js
@@ -1,41 +1,34 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
-// GET, POST, PUT , PATCH, DELETE
-
-// GET => Buscar uma informação do back-end
-// POST => Criar uma informação no back-end
-// PUT => Atualizar uma informação no back-end
-// PATCH => Atualziar uma informaçãao específica de uma recurso no back-end
-// DELETE => Deletar uma informação no back-end
-
-// Stateful - Stateless
-
-//Cabeçalhos (Requisições/resposta) => Metadados
-
-const users = []
+//Query Parameters
+//Route Parameters
+//Request Body
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
-        return res
-            .end(JSON.stringify(users))
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const routeParams = req.url.match(route.path)
+
+        // console.log(extractQueryParams(routeParams.groups.query))
+
+        const {query, ...params} = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
+
+        return route.handler(req, res)
     }
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-
-        users.push({
-            id: 1,
-            name,
-            email
-        })
-
-        return res.writeHead(201).end()
-    }
     return res.writeHead(404).end('Not Found!')
 })
 
